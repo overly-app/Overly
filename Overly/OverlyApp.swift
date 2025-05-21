91e81f6dac146c08472bc27da3211b17d3f05059
Hypackel <contact@hypackel.com>
2025-05-20 22:22:11 -0400

    Initial Commit

---
+++ Overly/OverlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverlyApp.swift
+//  Overly
+//
+//  Created by hypackel on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct OverlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
